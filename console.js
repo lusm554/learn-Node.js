19ce18d34000881c7e53cbfd609298f71419be0c
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-13 17:28:43 +0300

    time/timeEnd

--- console.js
+++ console.js
@@ -1,7 +1,7 @@
 // format specifier
 console.log('My %s has %d years and %d month obj --> %o', 'cat', 2, 3.2, {name: 'Masya', age: 2})
 
-// console.count()
+// console.count
 let x = 1
 for(let i = 1; i <= 5; i++) {
     console.count('how many times?', x)
@@ -13,8 +13,21 @@ arr.forEach(num => {
     console.count(num)
 })
 
-// console.trace()
+// console.trace
 const func1 = () => console.trace()
 const func2 = () => func1()
 
 func2()
+
+// console.time / console.timeEnd
+async function time() {
+    console.time('checkTime')
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('done')
+        }, 2000);
+    })
+    console.timeEnd('checkTime')
+}
+
+time()

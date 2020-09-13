939d074dee4a753ca06d418e7fdf35d2c3d28ce8
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-13 17:19:07 +0300

    console.count

--- console.js
+++ console.js
@@ -1,1 +1,14 @@
+// format specifier
 console.log('My %s has %d years and %d month obj --> %o', 'cat', 2, 3.2, {name: 'Masya', age: 2})
+
+// console.count()
+let x = 1
+for(let i = 1; i <= 5; i++) {
+    console.count('how many times?', x)
+}
+
+let arr = Array.from({ length: 10 }).fill('apple')
+
+arr.forEach(num => {
+    console.count(num)
+})

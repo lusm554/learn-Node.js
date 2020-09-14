d0a6b4c7a56818b159734ea7f372d5813c4f1ae1
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-14 18:23:34 +0300

    setInterval

--- timers.js
+++ timers.js
@@ -1,3 +1,10 @@
 const sum = (arg1, arg2) => console.log(arg1 + arg2)
 
 setTimeout(sum, 1000, 1, 2)
+
+let i = 1
+const log = () => console.log('hi', i++)
+
+let id = setInterval(log, 1000)
+
+setTimeout(() => clearInterval(id), 5500)

c39d92c7d73bddfddbafdb51890d80941662918d
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-15 19:32:50 +0300

    get req

--- http/httpsReq.js
+++ http/httpsReq.js
@@ -1,14 +1,16 @@
 // Makes all requests insecure
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const https = require('https')
-const options = {
+
+// GET request
+const get_options = {
     hostname: '127.0.0.1',
     port: 8080,
     path: '/something',
     method: 'GET'
 }
 
-const req = https.request(options, res => {
+const req = https.request(get_options, res => {
     console.log('Status code:', res.statusCode)
 
     res.on('data', d => {

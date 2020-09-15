7c3089f1d4ef5bededed653a1dcd335527d82d56
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-15 15:35:27 +0300

    error from promise

--- asyncAwait.js
+++ asyncAwait.js
@@ -3,3 +3,9 @@ const test = async () => {
 }
 
 test().then(console.log)
+
+const getError = async () => {
+    throw new Error('Error from getError')
+}
+
+getError().then(console.log, console.error)

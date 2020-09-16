a20476a6123788a8dcec966efa4f1c32a0fb33fb
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-16 21:40:56 +0300

    file name

--- fs/path.js
+++ fs/path.js
@@ -3,5 +3,8 @@ const path = require('path')
 const text = '../fs/path.js'
 
 console.log(path.dirname(text))
+// File name with ext 
 console.log(path.basename(text))
+// Just file name
+console.log(path.basename(text, path.extname(text)))
 console.log(path.extname(text))

7d83b3bd1cf6bead380ab1245203f974518eca04
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-16 22:00:19 +0300

    reading file sync

--- fs/readingWriting.js
+++ fs/readingWriting.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
+// Async version 
 fs.readFile(path.resolve('fs', 'text.txt'), 'utf-8', (err, text) => {
     if(err) {
         console.error(err)
@@ -8,3 +9,11 @@ fs.readFile(path.resolve('fs', 'text.txt'), 'utf-8', (err, text) => {
     }
     console.log(text)
 })
+
+//Sync version 
+try {
+    let text = fs.readFileSync(path.resolve('fs', 'text.txt'), 'utf-8')
+    console.log(text)
+} catch (error) {
+    console.error(error)
+}

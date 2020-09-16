3824e88ade11f6ab08593c554df2b2b5f0efc2ca
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-16 21:50:01 +0300

    path methods

--- fs/path.js
+++ fs/path.js
@@ -8,3 +8,21 @@ console.log(path.basename(text))
 // Just file name
 console.log(path.basename(text, path.extname(text)))
 console.log(path.extname(text))
+
+let name = 'Nikita'
+console.log(
+    path.join('..', 'users', name, 'text.txt')
+)
+
+console.log(
+    path.resolve('text.txt')
+)
+
+let folder = 'someFolder'
+console.log(
+    path.resolve(folder, 'text.txt')
+)
+
+console.log(
+    path.normalize('/users/who/../test.txt')
+)

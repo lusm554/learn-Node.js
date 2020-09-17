0c623addf50c39eb4182fb89a747392770261738
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-17 20:33:47 +0300

    events

--- events.js
+++ events.js
@@ -1,12 +1,38 @@
 const EventEmitter = require('events');
+const CLI_interface = require('readline').Interface({
+    input: process.stdin,
+    output: process.stdout
+})
 const eventEmitter = new EventEmitter()
 
-let i = 1;
+// let i = 1;
 
-eventEmitter.on('test', (...args) => {
-    console.log(`You are called the test ${i++} times, with the follow args ->`, args)
+// eventEmitter.on('test', (...args) => {
+//     console.log(`You are called the test ${i++} times, with the follow args ->`, args)
+// })
+
+// eventEmitter.emit('test', 1, 2, 3)
+// eventEmitter.removeAllListeners('test')
+// eventEmitter.emit('test', 4, 5, 6)
+
+eventEmitter.on('hi', (name) => {
+    console.log(`Hello ${name}`)
 })
 
-eventEmitter.emit('test', 1, 2, 3)
-eventEmitter.removeAllListeners('test')
-eventEmitter.emit('test', 4, 5, 6)
+eventEmitter.on('hi', (name) => {
+    console.log(`Bye ${name}`)
+    CLI_interface.pause()
+})
+
+eventEmitter.prependListener('user', (name) => {
+    CLI_interface.question('What is your name?', (name) => {
+        eventEmitter.emit('hi', name)
+    })
+})
+
+eventEmitter.emit('user')
+
+// eventEmitter.eventNames()
+// eventEmitter.listenerCount('hi')
+// Return all listeners 
+// eventEmitter.listeners('hi')
